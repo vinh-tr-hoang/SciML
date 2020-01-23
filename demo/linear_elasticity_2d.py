351ba54a43cb1ec7a74f0a247bb5513ae9b4e72a
vinh-tr-hoang <vinhvinhtruong@gmail.com>
2020-01-23 17:54:38 +0100

    convergence check

--- demo/linear_elasticity_2d.py
+++ demo/linear_elasticity_2d.py
@@ -20,44 +20,36 @@ ub=1.
 x=[np.linspace(lb, ub, 100, dtype='float64') for i in range(dim)]
 X=np.meshgrid(*x) # for plotting np.meshgrid(x[0], x[1])
 X_plot=np.hstack([X[i].reshape(X[0].size, 1) for i in range(dim)])
-layers=[2, 20, 20, 2]
+nbnr = 5
+layers=[2, nbnr, nbnr, 2]
 
-colocation=lhs(n=dim, samples=2000)
+
 # Dirichlet boundary conditions
-xDL=np.zeros(shape=(20, 1))
-yDL=np.linspace(0., 1., 20).reshape(xDL.shape)
-xD=np.hstack([xDL, yDL])
-uD=np.zeros ((20, 2))
+xdl = np.zeros(shape=(20, 1))
+xdr = np.ones(shape=(20, 1))
+xd = np.concatenate((xdl, xdr))
+ydl = np.linspace(0., 1., 20).reshape(xdl.shape)
+yd = np.concatenate((ydl, ydl))
+xD=np.hstack([xd, yd])
+uD=np.zeros ((40, 2))
+uD[20:,0] = 0.03
 
-# Neumann boundary conditions
-xN=np.hstack([np.ones(shape=(20, 1)), np.linspace(0., 1., 20).reshape((20, 1))])
-fN=np.ones ((20, 2))*30./20.
-fN[:, 1]=0.
+# Neumann boundary conditions (no applied forces)
+xN=np.hstack([np.ones(shape=(1, 1)), np.linspace(0., 1., 1).reshape((1, 1))])
+fN=np.zeros ((1, 2))
+#fN[:, 1]=0.
+batchsize = 10*(layers[1]*(layers[0]*2 + 1) + layers[2]*(layers[1]*2 + 1) + layers[3]*(layers[2]*2 + 1))
+nbcolopoint = batchsize*20
+colocation=lhs(n=dim, samples=nbcolopoint)
 
-for globalsearch_i in range (10):
+epoch = 1000
+for globalsearch_i in range (1):
     model=LinearElasticity(dim, layers, lb, ub)
-    # model.weights = model.weights*0.
-    # model.biases = model.biases*0.
-#        u = model.net_u(colocation)
-#        epsilon = model.net_epsilon(colocation)
-#        net_stress = model.net_stress(colocation)
-
+    model.initialize_NN(model.layers, sigma = 0.3)
     elastic_energy=model.elastic_energy(colocation, xD, uD, xN, fN, verbose=True)
-    lr=1.e-1
+    lr=5.e-2
     opt=tf.keras.optimizers.Adam(learning_rate=lr)
-    for i in range (0, 100):
-        # stochastic gradient descent
-        fN1=np.zeros ((10, 2))*10.
-        ith=np.random.choice(1000, size=50, replace=None)
-        x_batch=colocation[ith]
-        if i % 1 == 0:
-            print ('step ', i)
-            stop_flag=model.train_step(colocation, xD, uD, xN, fN, opt, verbose=True, learning_rate=lr)
-            if stop_flag and i>=10:
-                break
-        else:
-            stop_flag=model.train_step(x_batch, xD, uD, xN, fN, opt, verbose=False, learning_rate=lr)
-
+    model.stochastic_gradient_descent (colocation, xD, uD, xN, fN, opt, epoch, batchsize, verbose=True, learning_rate= lr)
     if globalsearch_i==0:
         lost_old=model.elastic_energy(colocation, xD, uD, xN, fN, verbose=True)
         model_old=model
